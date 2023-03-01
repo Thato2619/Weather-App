dbb796689e0391929035747a11764ac8d81d8b4f
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-03-01 23:07:54 +0200

    import the all jpeg images in the images folder
    - add inline styling for the background images
    
    - add 3 more divs and add input

--- client-experience/src/App.js
+++ client-experience/src/App.js
@@ -1,10 +1,18 @@
 
 import './App.css';
+import sunnyDay from './images/sunny-day.jpeg';
+import coldDay from './images/cold-day.jpeg';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="App" style={{backgroundImage: `url(${coldDay})`}}>
+      <div className='overlay'>
+        <div className='container'>
+          <div className='section section__inputs'>
+            <input type="text" name='city'/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
