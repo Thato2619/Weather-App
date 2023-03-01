d316ec060a2aa08573e180e6ba5e94a25a98a18f
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-03-02 01:43:36 +0200

    import descriptions.css and import the react-icons
    - add divs with classname for the design of Description.jsx

--- client-experience/src/components/description/Descriptions.jsx
+++ client-experience/src/components/description/Descriptions.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import './descriptions.css';
+import {faArrowDown} from "react-icons/fa";
 
 const Descriptions = () => {
   return (
-    <div>Descriptions</div>
+    <div className='section section__descriptions'>
+        <div className='card'>
+            <div className='description__card-icon'>
+                <faArrowDown />
+                <small>min</small>
+            </div>
+            <h2>32°C</h2>
+        </div>
+    </div>
   )
 }
 
