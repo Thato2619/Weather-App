4d34798ac3ee2f6da7f402ffc364ebf0be3177e3
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-03-02 22:07:11 +0200

    try to add login and registration page
    - remove modules that are not needed in the json file
    
    - add a ReadMe that speaks about everything involved in the project

--- api/index.js
+++ api/index.js
@@ -6,19 +6,20 @@ const request = require('request');
 const app = express();
 
 app.use(express.json()); 
+app.use(request.json()); //use request within json package
 
 app.listen(3000, ()=>{
     console.log("Connected!")
 })
 
-/*app.get('/', (req,res) => {
+app.get('/', (req,res) => {
     let city = req.query.city;
     const request = require('request');
-request(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e1745f274f772287c0d082209965c8ef`, 
+request(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e1745f274f772287c0d082209965c8ef`, //request api by using HTTP get method
 function (error, response, body) {
      let data = JSON.parse(body)
     if(response.statusCode === 200) {
-        res.send(`The weather in your city "${city}" is "${body.list[0].weather[0].description}"`)
+        res.send(`The weather in your city "${city}" is "${body.list[0].weather[0].description}"`) //response to the request from the api
     }
   console.error('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
@@ -27,5 +28,5 @@ function (error, response, body) {
 
 })
 
-app.listen(3000, () => console.log('server started on port 3000'));*/
+app.listen(3000, () => console.log('server started on port 3000'));
 
---
+++ client-experience/MoreAboutProject.md
@@ -0,0 +1,29 @@
+# The Full Stack Weather App
+
+This project consistent with mainlt react, a bit of javascript and node.
+
+The basis of this app tells the weather with the help of an API  from openweathermap
+
+please see the link: https://openweathermap.org/api 
+
+# Node
+I hope that i applied what you were explaining during our lesson earlier
+
+# Biblilography and References
+This app i was truly inspired by the following: 
+ https://openweathermap.org/api 
+ https://www.youtube.com/watch?v=nJMOTG-dfQ0 
+ https://www.youtube.com/watch?v=D32qawkUxF8&t=73s
+ https://www.youtube.com/watch?v=7SmMUS3rWjs 
+
+
+
+## The issues with app so far
+
+Everytime i attempt to create a login and registration form, it immediately breaks my code.
+
+To the very point where, React fails to compile deleted that contained Login and Registration files
+
+I don't know what I did but its quite weird
+
+### `npm start`
